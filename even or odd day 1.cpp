7b753c04e1391007903938ca37887329ec59a8a8
Dharshh30 <112070627+Dharshh30@users.noreply.github.com>
2023-04-12 21:05:47 +0530

    Add files via upload

---
+++ even or odd day 1.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+int main() {
+int number;
+cout<<"enter a number:"<<endl;
+cin>>num;
+if(a%2 == 0) {
+ cout<<"even";}
+else {
+    cout<<"odd";}
+return 0;
+}
